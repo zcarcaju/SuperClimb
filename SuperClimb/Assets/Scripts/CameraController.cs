74d42c4d55151d83d71cc06aa6b8e5356475806e
Isaque <contato@isaqueoliva.com>
2019-06-15 02:29:50 -0300

    Initial Project

---
+++ SuperClimb/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private float moveSpeed = 0f;
+    public Transform target; // Drop the player in the inspector of the camera
+
+    void Update()
+    {
+        Vector3 newPosition =
+        transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+    }
+}
---
+++ SuperClimb/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    float x = 0;
+    float y=0;
+    private float jSpeed = 4f;
+    private float mSpeed = 4f;
+    private bool JumpNow = false;
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    private void FixedUpdate()
+    {
+        Jump();
+    }
+    private void Update()
+    {
+        
+        IsJumping();
+    }
+    private void IsJumping()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            JumpNow = true;   
+        } 
+    }
+    private void Jump()
+    {
+        if (JumpNow)
+        {
+            
+            
+            rb.velocity = new Vector2(mSpeed, jSpeed);
+            mSpeed *= -1;
+            JumpNow = false;
+        }
+    }
+}
