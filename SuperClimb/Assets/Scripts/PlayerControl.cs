9e13e711552d516be17021ce1fcaae4f02274966
Isaque Oliveira <contato@isaqueoliveira.com>
2019-06-16 01:51:14 -0300

    Update 1.0
    Double Jump, JumpCount condition, Wall collides reset JumpCount, Portals change sprite

--- SuperClimb/Assets/Scripts/PlayerControl.cs
+++ SuperClimb/Assets/Scripts/PlayerControl.cs
@@ -2,35 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    float x = 0;
-    float y=0;
-    private float jSpeed = 4f;
-    private float mSpeed = 4f;
+    private float jSpeed = 5f;
+    private float mSpeed = 6f;
+    private int countJump = 0;
+    private int maxCountJump = 2;
     private bool JumpNow = false;
+
     private Rigidbody2D rb;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        //gameObject.GetComponent<Renderer>().enabled = true;
+        spriteRenderer.enabled = true;
     }
     private void FixedUpdate()
     {
         Jump();
+        
     }
     private void Update()
     {
-        
         IsJumping();
+        
     }
     private void IsJumping()
     {
-        
+  
         if (Input.GetMouseButtonDown(0))
         {
             JumpNow = true;   
@@ -38,13 +42,49 @@ public class PlayerControl : MonoBehaviour
     }
     private void Jump()
     {
-        if (JumpNow)
+        if (JumpNow && countJump < maxCountJump)
         {
-            
-            
             rb.velocity = new Vector2(mSpeed, jSpeed);
             mSpeed *= -1;
+            countJump++;
             JumpNow = false;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //BLUE   
+        if (collision.gameObject.tag == "portalB" && spriteRenderer.enabled == false)
+        {
+            //spriteRenderer.enabled = true;
+
+        }
+        if (collision.gameObject.tag == "portalB" && spriteRenderer.enabled == true)
+        {
+            spriteRenderer.enabled = false;
+
+        }
+        //ORANGE
+
+        if (collision.gameObject.tag == "portalO" && spriteRenderer.enabled == true)
+        {
+            //spriteRenderer.enabled = true;
+
+        }
+        if (collision.gameObject.tag == "portalO" && spriteRenderer.enabled == false)
+        {
+            spriteRenderer.enabled = true;
+
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "oSide" || collision.gameObject.tag == "bSide")
+        {
+            ResetJump();
+        }
+    }
+    private void ResetJump()
+    {
+        countJump = 0;
     }
 }
