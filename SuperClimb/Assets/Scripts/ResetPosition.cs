17ee541ddb131bbf0449422e49a1d854a83e70bf
Isaque Oliveira <contato@isaqueoliveira.com>
2019-07-14 11:11:30 -0300

    Update 2.0
    Fixed start bug. Beta build ok

---
+++ SuperClimb/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("portalO") || collision.gameObject.CompareTag("portalB"))
+        {
+            
+            collision.gameObject.transform.position = new Vector2(0f, 325f);
+        }
+    }
+}
